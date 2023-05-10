3ab352c87e39502cadf26a3bec434849833b4c27
Phill <phillipwood01@gmail.com>
2023-05-10 12:45:00 +0100

    idk

--- test.py
+++
@@ -1,6 +0,0 @@
-import os
-filepath = os.path.dirname(__file__)+r"\\"
-logspath = os.path.join(filepath, "Logs")
-teampath = os.path.join(filepath, "Teams")
-indivpath = os.path.join(filepath, "Indivs")
-print(filepath, "\n\n", logspath,"\n\n", teampath,"\n\n", indivpath)
